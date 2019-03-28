a172346851acd5ac58a65aa717febedd37bb1585
cristopher@asimmetrica.com <cristopher@asimmetrica.com>
2019-03-28 02:13:00 +0100

    Fix toggle e stati degli irrigatori

--- src/app/devices.service.ts
+++ src/app/devices.service.ts
@@ -7,56 +7,52 @@ import { BehaviorSubject, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class DevicesService {
-
   pinstatus: BehaviorSubject<any> = new BehaviorSubject(null);
 
   pinstatus1: BehaviorSubject<any> = new BehaviorSubject(null);
   pinstatus2: BehaviorSubject<any> = new BehaviorSubject(null);
   pinstatus3: BehaviorSubject<any> = new BehaviorSubject(null);
 
-  constructor(
-    private http: Http
-  ) {
-
-   }
+  constructor(private http: Http) {}
 
   getBordStatus() {
-    this.http.get('http://localhost:8080/getstatus', '' ).pipe(
-      map(res => res.json())
-    ).subscribe(response => {
-      this.pinstatus.next(response);
-    });
+    this.http
+      .get('http://192.168.1.109:8080/getstatus', '')
+      .pipe(map(res => res.json()))
+      .subscribe(response => {
+        this.pinstatus.next(response);
+      });
   }
 
   fetchPin(id) {
-    this.http.get('http://localhost:8080/getstatus', id ).pipe(
-      map(res => res.json())
-    ).subscribe(response => {
-
-      if (id === 1) {
-        this.pinstatus1.next(response);
-      } else if (id === 2) {
-        this.pinstatus2.next(response);
-      } else if (id === 3 ) {
-        this.pinstatus3.next(response);
-      }
-
-    });
+    this.http
+      .get('http://192.168.1.109:8080/getstatus', id)
+      .pipe(map(res => res.json()))
+      .subscribe(response => {
+        if (id === 1) {
+          this.pinstatus1.next(response);
+        } else if (id === 2) {
+          this.pinstatus2.next(response);
+        } else if (id === 3) {
+          this.pinstatus3.next(response);
+        }
+      });
   }
 
-  async togglePin( id: number ) {
-    this.http.post('http://localhost:8080/toggle' + id , '' ).pipe(
-      map(res => res.json())
-    ).subscribe(async response => {
-      if (response.pinId === 1) {
-        this.pinstatus1.next(response.state);
-      } else if (response.pinId === 2) {
-        this.pinstatus2.next(response.state);
-      } else if (response.pinId === 3) {
-        this.pinstatus3.next(response.state);
-      }
-      this.getBordStatus();
-      return response;
-    });
+  async togglePin(id: number) {
+    this.http
+      .post('http://192.168.1.109:8080/toggle' + id, '')
+      .pipe(map(res => res.json()))
+      .subscribe(async response => {
+        if (response.pinId === 1) {
+          this.pinstatus1.next(response.state);
+        } else if (response.pinId === 2) {
+          this.pinstatus2.next(response.state);
+        } else if (response.pinId === 3) {
+          this.pinstatus3.next(response.state);
+        }
+        this.getBordStatus();
+        return response;
+      });
   }
 }
